c697ec0c05a647be5284b0540aea79dff9c5ed13
user1990 <paulius.rimg1990@gmail.com>
2017-05-22 14:53:05 +0300

    # Update 1.4

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 const app = {};
 
-app.apiUrl = 'https://api.spotify.com/v1/search';
+app.apiUrl = 'https://api.spotify.com/v1/';
 
 // Allow the user to enter some names
 app.events = function() {
   $('form').on('submit', function(e) {
     e.preventDefault();
+    $('.loader').toggleClass('show');
     let artists = $('input[type=search]').val();
     artists = artists.split(','); // Create array
     let search = artists.map((artistName) => app.searchArtist(artistName));
 
-    $.when(...search)
-      .then((...results) => {
-        results = results.map(res => res[0].artists.items[0].id);
-        console.log(results);
-      });
+    app.retrieveArtistInfo(search);
 
   });
 };
@@ -25,23 +22,80 @@ app.searchArtist = (artistName) => $.ajax({
   method: 'GET',
   dtaType: 'json',
   data: {
-    q: artistName,
-    type: 'artist'
+    type: 'artist',
+    q: artistName
   }
 });
 
 // Get albums by id
-/* app.getArtistAlbums = (artistId) => $.ajax({
-  url: 'GET',
+app.getArtistAlbums = (artistId) => $.ajax({
+  url: `${app.apiUrl}/artists/${artistId}/albums`,
+  method: 'GET',
   dataType: 'json',
   data: {
     album_type: 'album'
   }
-});*/
+});
 
 // Get tracks
+app.getArtistTracks = (id) => $.ajax({
+  url: `${app.apiUrl}/albums/${id}/tracks`,
+  method: 'GET',
+  dataType: 'json'
+});
 
 // Build playlist
+app.buildPlayList = function(tracks) {
+  $.when(...tracks)
+    .then((...tracksResults) => {
+      tracksResults = tracksResults.map(getFirstElement)
+        .map(item => item.items)
+        .reduce(flatten, [])
+        .map(item => item.id);
+
+      const randomTracks = [];
+      for (let i = 0; i < 30; i++) {
+        randomTracks.push(getRandomTrack(tracksResults));
+      }
+      const baseUrl = `https://embed.spotify.com/?theme=white&uri=spotify:trackset:My Playlist:${randomTracks.join()}`;
+
+      $('.loader').toggleClass('show');
+      $('.playlist').html(`<iframe src="${baseUrl}" height="400"></iframe>`);
+
+    });
+};
+
+app.retrieveArtistInfo = function(search) {
+  $.when(...search)
+      .then((...results) => {
+        results = results.map(getFirstElement)
+          .map(res => res.artists.item[0].id)
+          .map(id => app.getArtistAlbums(id));
+
+        app.retrieveArtistTracks(results);
+      });
+};
+
+app.retrieveArtistTracks = function(artistAlbums) {
+  $.when(...artistAlbums)
+    .then((...albums) => {
+      let albumIds = albums.map(getFirstElement)
+        .map(res => res.items)
+        .reduce(flatten, [])
+        .map(album => album.id)
+        .map(ids => app.getArtistTracks(ids));
+      app.buildPlayList(albumIds);
+    });
+};
+
+const getFirstElement = (item) => item[0];
+
+const flatten = (prev, curr) => [...prev, ...curr];
+
+const getRandomTrack = (trackArray) => {
+  const randomNum = Math.floor(Math.random() * trackArray.length);
+  return trackArray[randomNum];
+};
 
 app.init = function() {
   app.events();
